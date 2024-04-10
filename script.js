d5724664b5ff6839e9f7327d68253e5412afb2ac
TheCoderRajat07 <rajatkhairnar22@gmail.com>
2024-04-10 21:04:10 +0530

    errors solved

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 gsap.fromTo(
     ".loading-page",
@@ -24,31 +23,4 @@ gsap.fromTo(
       delay: 0.5,
     }
   );
-=======
-
-gsap.fromTo(
-    ".loading-page",
-    { opacity: 1 },
-    {
-      opacity: 0,
-      display: "none",
-      duration: 1.5,
-      delay: 3.5,
-    }
-  );
-  
-  gsap.fromTo(
-    ".logo-name",
-    {
-      y: 50,
-      opacity: 0,
-    },
-    {
-      y: 0,
-      opacity: 1,
-      duration: 2,
-      delay: 0.5,
-    }
-  );
->>>>>>> 7024404de897d9bb3b185e4f97e2e9f88f0b944c
   
